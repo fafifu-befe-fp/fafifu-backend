d9af485372518b24174f166568edac3866d0561e
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-06-15 12:22:13 +0700

    feat: add middlewares
    +authorization
    +errorHandler
    +validation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./api/routes/index");
-var usersRouter = require("./api/routes/users");
+const indexRouter = require("./api/routes/index");
+const usersRouter = require("./api/routes/users");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,5 +16,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/produk", produkRouter);
 
 module.exports = app;
