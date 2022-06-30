2346d2555797a6246634789f6baabb0c855a701e
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-06-30 19:41:52 +0700

    feat: init swagger documentation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const errorHandler = require("./api/v1/middlewares/errorHandler");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./api-docs/swagger.json");
 
 const {
   userRouter,
@@ -18,7 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
+app.use("/api-docs", swaggerUi.serve);
 
+app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 app.use("/v1/user", userRouter);
 app.use("/v1/auth", authRouter);
 app.use("/v1/product", productRouter);
