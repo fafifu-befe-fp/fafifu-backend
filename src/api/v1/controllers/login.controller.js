1e98423d9cab45102377be5d49189dd9ec824fec
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-07-04 20:57:51 +0700

    minor: add user data after login

--- src/api/v1/controllers/login.controller.js
+++ src/api/v1/controllers/login.controller.js
@@ -8,18 +8,37 @@ class LoginController {
 
       if (user) {
         if (await comparePassword(req.body.password, user.password)) {
+          const dataUser = await User.findOne({
+            attributes: ["id", "email"],
+            where: { id: user.id },
+            include: [
+              {
+                model: UserBiodata,
+                attributes: [
+                  "name",
+                  "imageUrl",
+                  "city",
+                  "address",
+                  "handphone",
+                ],
+              },
+            ],
+          });
           res.status(200).json({
             data: {
               token: await generateJWT(user.publicId, user.email),
+              user: {
+                id: dataUser.id,
+                name: dataUser.UserBiodatum.name,
+                email: dataUser.email,
+                imageUrl: dataUser.UserBiodatum.imageUrl,
+                city: dataUser.UserBiodatum.city,
+                address: dataUser.UserBiodatum.address,
+                handphone: dataUser.UserBiodatum.handphone,
+              },
             },
           });
         } else {
-          throw {
-            status: 401,
-            message: "Invalid email or password",
-          };
-        }
-      } else {
         throw {
           status: 401,
           message: "Invalid email or password",
