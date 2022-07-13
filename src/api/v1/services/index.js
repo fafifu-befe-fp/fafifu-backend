668c9ff4819b87c1093e158fcae7e41e1025ba06
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-07-13 13:52:07 +0700

    feat: add column is published and is available

--- src/api/v1/services/index.js
+++ src/api/v1/services/index.js
@@ -1,9 +1,12 @@
+"use strict";
 const UserService = require("./user.services");
 const ProductService = require("./product.services");
 const WishlistService = require("./wishlist.services");
+const NotificationService = require("./notification.services");
 
 module.exports = {
   UserService,
   ProductService,
   WishlistService,
+  NotificationService,
 };
