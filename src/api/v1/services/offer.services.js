04dfe13841d22621b33b2772d0e4f31036b3d221
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-07-19 20:55:21 +0700

    feat: sold product list

--- src/api/v1/services/offer.services.js
+++ src/api/v1/services/offer.services.js
@@ -66,6 +66,65 @@ class OfferService {
     });
   }
 
+  static async getSoldProduct(userIdParam) {
+    return (
+      await Offer.findAll({
+        attributes: ["publicId", "price"],
+        include: [
+          {
+            model: Product,
+            attributes: ["publicId", "name", "price"],
+            include: [
+              {
+                model: ProductCategory,
+                attributes: ["id"],
+                include: {
+                  model: Category,
+                  attributes: ["name"],
+                },
+              },
+              {
+                model: ProductImage,
+                attributes: ["imageUrl"],
+              },
+            ],
+            where: {
+              userId: userIdParam,
+            },
+          },
+          {
+            model: UserBiodata,
+            attributes: ["name"],
+          },
+        ],
+        order: [
+          [Product, ProductImage, "id", "ASC"],
+          [Product, ProductCategory, "id", "ASC"],
+        ],
+        where: {
+          statusOfferId: 3,
+        },
+      })
+    ).map((item) => {
+      return {
+        offerPublicId: item.publicId,
+        productPublicId: item.Product.publicId,
+        name: item.Product.name,
+        description: item.Product.description,
+        productPrice: item.Product.price,
+        offerPrice: item.price,
+        category: item.Product.ProductCategories.map((item) => {
+          return {
+            id: item.id,
+            name: item.Category.name,
+          };
+        }),
+        imageUrl: item.Product.ProductImages[0].imageUrl,
+        buyerName: item.UserBiodatum.name,
+      };
+    });
+  }
+
   static async isOfferExists(buyerIdParam, productIdParam) {
     return await Offer.findOne({
       where: {
