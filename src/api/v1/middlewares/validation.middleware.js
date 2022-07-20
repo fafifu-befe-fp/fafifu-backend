e1960d42d2376c72f849183ca1743abe60c4a5a3
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-07-20 10:51:26 +0700

    major: middleware validation

--- src/api/v1/middlewares/validation.middleware.js
+++ src/api/v1/middlewares/validation.middleware.js
@@ -3,13 +3,10 @@ const { validationResult } = require("express-validator");
 
 module.exports = (req, res, next) => {
   const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
-  const extractedErrors = [];
-  errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
 
-  return res.status(422).json({
-    errors: extractedErrors,
-  });
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
+  }
+  return next();
 };
