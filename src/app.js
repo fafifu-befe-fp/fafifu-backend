a9ddb87052d69cd542d8ad9861342b452f542a19
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-07-23 22:13:39 +0700

    minor: correct typo Sentry

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use((req, res, next) => {
-  req.sentry = sentry;
+  req.sentry = Sentry;
   next();
 });
 app.use("/api-docs", swaggerUi.serve);
